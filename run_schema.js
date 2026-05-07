3dac527c37bc72258f47cd347fb831777f7a6e62
BallDevTools <ballzsilachai@gmail.com>
2026-05-07 21:11:40 +0700

    tmp: fix schema runner skip USE/CREATE DATABASE

--- run_schema.js
+++ run_schema.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const fs = require('fs')
 const db = require('./db')
 const sql = fs.readFileSync('./migrations/schema.sql', 'utf8')
-const stmts = sql.split(';').map(s => s.trim()).filter(s => s && s.indexOf('--') !== 0)
+const stmts = sql.split(';').map(s => s.trim()).filter(s => s && s.indexOf('--') !== 0 && s.toUpperCase().indexOf('USE ') !== 0 && s.toUpperCase().indexOf('CREATE DATABASE') !== 0)
 ;(async () => {
   for (const s of stmts) {
     try { await db.query(s) } catch(e) {

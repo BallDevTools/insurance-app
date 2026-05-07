8c6b5537470c0670be61409c86fd9b3dcba41485
BallDevTools <ballzsilachai@gmail.com>
2026-05-07 21:08:06 +0700

    tmp: schema runner

---
+++ run_schema.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const fs = require('fs')
+const db = require('./db')
+const sql = fs.readFileSync('./migrations/schema.sql', 'utf8')
+const stmts = sql.split(';').map(s => s.trim()).filter(s => s && s.indexOf('--') !== 0)
+;(async () => {
+  for (const s of stmts) {
+    try { await db.query(s) } catch(e) {
+      if (e.message.indexOf('already exists') === -1) console.log('skip:', e.message.substring(0, 80))
+    }
+  }
+  console.log('schema done')
+  process.exit(0)
+})()
